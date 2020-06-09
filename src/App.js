f071edb116e3ba2b772a985e7fa281370e3ef3b5
Tevvin <Tevvin7395@icloud.com>
2020-06-08 21:45:19 -0400

    uuid is working now

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import React from 'react'; /* Importing React */
 import Header from './components/layout/header.js' /* Importing Header Component */
 import AddTodo from './components/todos/AddTodo' /* Importing AddTodo Component */
 import Todos from './components/todos/Todos.js' /* Importing Todos component */
+import { v4 as uuidv4 } from 'uuid';
 
 import './App.css'; /* Importing App.css */
 
-//TODO: uuid is giving type error: TypeError: uuid__WEBPACK_IMPORTED_MODULE_4__.v4.v4 is not a function. Work Tomorrow!!!!!!
 class App extends React.Component {
   state = { /* State is a object that holds an array of objects */
     todos: [
       {
-        id: uuidv4.v4(),
+        id: uuidv4(),
         title: "Take out the trash",
         completed: false
       },
       {
-        id: uuidv4.v4(),
+        id: uuidv4(),
         title: "Take Bhodi out for a walk",
         completed: false
       },
       {
-        id: uuidv4.v4(),
+        id: uuidv4(),
         title: "Get more dog food for Bhodi",
         completed: false
       }
@@ -44,7 +44,7 @@ class App extends React.Component {
   //Add Todo Item
   addTodo = (title) => {
     const newTodo = {
-      id: uuidv4.v4(),
+      id: uuidv4(),
       title: title,
       completed: false
     }
